1166e469b7c622aca98172d732925278900729c7
Martín <marto2286@gmail.com>
2021-10-06 10:50:23 +0200

    StartCode

---
+++ EventLogsReport/EventLogsReport/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace EventLogsReport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Read Event Log";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(421, 426);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(22, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Read Task Scheduler";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Les incentives SAP to SQL",
+            "DM_LES_SAP_TO_SQL",
+            "ES_DM_LOAD_CONTROLLING_SAP_TO_SQL",
+            "ES_LOAD_LOGISTICS",
+            "LES_CRM",
+            "VIS_MEUGER_LOAD",
+            "DM_LES_SAP_TO_SQL_DELTA",
+            "DM_LES_SAP_TO_SQL_DAILY",
+            "Adobe Acrobat Update Task",
+            "GoogleUpdateTaskMachineUA"});
+            this.comboBox1.Location = new System.Drawing.Point(22, 143);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(220, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(220, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(439, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(263, 190);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ EventLogsReport/EventLogsReport/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventLogsReport
+{
+    public partial class Form1 : Form
+    {
+        private string hostName = "" ;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            CheckEvenLogs();
+        }
+
+        private void CheckEvenLogs()
+        {
+            EventLog[] remoteEventLogs;
+            EventLog myEventLog = new EventLog("System", ".");
+
+            remoteEventLogs = EventLog.GetEventLogs("MININT-5LR42B8");
+
+            richTextBox1.AppendText("Number of logs on computer: " + myEventLog.Entries.Count + " \r");
+
+            int i = 0;
+            foreach (EventLogEntry log in myEventLog.Entries)
+            {
+                if (i < 200 && log.EntryType.ToString().Equals("Error"))
+                {
+                    richTextBox1.AppendText(log.Source + " \r");
+                    i++;
+                }
+                //Console.WriteLine("Log: " + log.Log);
+
+            }
+        }
+        private void CheckTaskScheduler(string TaskName)
+        {
+            using (var session = new EventLogSession(hostName))
+            {
+                var list = GetCompletedScheduledTaskEventRecords(session, TaskName)
+                    .OrderByDescending(x => x.TimeCreated)
+                    .Select(r => new {   id = r.Id, level = r.LevelDisplayName,CompletedTime = r.TimeCreated, r.TaskDisplayName, Props = string.Join(" | ", r.Properties.Select(p => p.Value)) });
+                for (var c = 0; c < list.Count(); c++)
+                {
+                    var ele = list.ElementAt(c);
+                    if(ele.id != 102)
+                        richTextBox1.SelectionColor = Color.Red;
+                    richTextBox1.AppendText(ele.CompletedTime.ToString() + " | " + ele.TaskDisplayName + " | " + ele.level + " | " + ele.id +  " \r");
+                    richTextBox1.SelectionColor = Color.Black;
+
+                }
+
+
+                //richTextBox1.dum(list);
+                //.Dump("Said Tasks Completed"); //using linqpad's Dump method, this just outputs the results to the display
+            }
+
+        }
+
+        //If you don't want completed tasks remove the second part in the where clause
+        private List<EventRecord> GetCompletedScheduledTaskEventRecords(EventLogSession session, string scheduledTask)
+        {
+            const int TASK_COMPLETED_ID = 102;
+            const int TASK_ERROR_ID = 101;
+            var logquery = new EventLogQuery("Microsoft-Windows-TaskScheduler/Operational", PathType.LogName, "*[System/Level=4]") { Session = session };
+            return GetRecords(logquery,
+                x => x.Properties.Select(p => p.Value).Contains($@"\{scheduledTask}") && (x.Id == TASK_COMPLETED_ID || x.Id == TASK_ERROR_ID)).ToList();
+        }
+        private IEnumerable<EventRecord> GetRecords(EventLogQuery query, Func<EventRecord, bool> filter)
+        {
+            using (var reader = new EventLogReader(query))
+            {
+                for (var record = reader.ReadEvent(); null != record; record = reader.ReadEvent())
+                {
+                    if (!filter(record)) continue;
+
+                    yield return record;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            CheckTaskScheduler(comboBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hostName = System.Environment.MachineName;
+            textBox1.Text = hostName;
+        }
+    }
+}
